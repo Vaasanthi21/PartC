f4581b04dc1c7b9751dab026d8df3b75886876bb
Vaasanthi21 <167667051+Vaasanthi21@users.noreply.github.com>
2025-11-23 19:28:50 +0530

    Implement counter function with increment logic
    Fix counter function to return an inner function that increments count.

---
+++ Question10.js
@@ -0,0 +1,11 @@
+function counter(){
+  let count=0;
+  return function(){
+    count=count+1;
+    return count;
+  };
+}
+const c = counter();
+console.log(c());
+console.log(c());
+console.log(c());
